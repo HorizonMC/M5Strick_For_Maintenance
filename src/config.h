e3af64b601fa774de24eae7291ee5d78143ded49
pongpiphat <pongpiphat.tach@gmail.com>
2019-04-11 21:26:57 +0700

    disable Sleep mode For test MPU 9250

---
+++ src/config.h
@@ -0,0 +1,13 @@
+#ifndef _CONFIG_H_
+#define _CONFIG_H_
+
+static const char *TAG = "example";
+
+const char* ssid       = "Horizon";
+const char* password   = "1q2w3e4r";
+
+
+#define HUNDRED_NANO_SECONDS (1000*1000*10)
+const long  gmtOffset_sec = 3600 * 8;
+
+#endif // _CONFIG_H_
